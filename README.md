edd884c6ac768299828c62955d46cb458b084de7
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-07-19 14:13:39 -0700

    got some geocode stuff working

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Resources:
 
 1. 5-day forecast: https://openweathermap.org/forecast5
-2. Geocoding API: https://openweathermap.org/api/geocoding-api
-3. Search history: https://stackoverflow.com/questions/35645373/is-there-any-jquery-plugin-available-to-show-recent-search-history
-4. Gradient maker: https://cssgradient.io/
+2. Current weather: https://openweathermap.org/weather-conditions
+3. Geocoding API: https://openweathermap.org/api/geocoding-api
+4. Search history: https://stackoverflow.com/questions/35645373/is-there-any-jquery-plugin-available-to-show-recent-search-history
+5. Gradient maker: https://cssgradient.io/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,47 @@
 var apiKey = 'fd5bcdf5b68d3fbf74e736379ffe6e3c';
 
+
+
+var searchBar = document.querySelector('#city-searchbar');
+var searchButton = document.querySelector('#city-search-submit-btn');
+
+
+searchButton.addEventListener('click', geocode);
+
+
+function geocode(event) {
+    
+    event.preventDefault();
+    var city = searchBar.value;
+    var geocodeUri = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    var 
+    var latLong = [];
+
+    console.log(city);
+    console.log(geocodeUri);
+    fetch (geocodeUri) 
+
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function(data) {
+
+            if (data.length === 0) {
+                console.log("Please enter a valid city")
+                return;
+            }
+            else {
+                console.log(data);
+                latLong[0] = data[0].lat;
+                latLong[1] = data[0].lon;
+                console.log(latLong);
+                return latLong;
+            }
+        })
+        .then(function(coordinates) {   
+            fetch()
+        })
+    searchBar.value = "";
+    return latLong;   
+}
