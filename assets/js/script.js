0fbf973f41ce593a12f2a8e4c7049c335322631d
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-07-20 10:28:11 -0700

    adding current day and refactoring stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,44 @@
 var apiKey = 'fd5bcdf5b68d3fbf74e736379ffe6e3c';
 var endpoint = 'http://api.openweathermap.org'
 
+function currentDate () {
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    // This arrangement can be altered based on how we want the date's format to appear.
+    let currentDate = `${month}/${day}/${year}`;
+    return currentDate;
+}
+
+
+
 
 var searchBar = document.querySelector('#city-searchbar');
 var searchButton = document.querySelector('#city-search-submit-btn');
 var forecastSection = document.querySelector('#forecast-section');
+var cityDateText = document.querySelector('#cityDate');
 
-searchButton.addEventListener('click', geocode);
+searchButton.addEventListener('click', function (event) {
+    geocode(event);
+    currentWeather(event);
+}
+);
 
+function currentWeather(event) {
+
+}
 
 function geocode(event) {
     
     event.preventDefault();
     var city = searchBar.value;
-    var geocodeUri = `${endpoint}/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+    var geocodeUri = `${endpoint}/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
     
     var latLong = [];
-
+    var weather = {};
     console.log(city);
     console.log(geocodeUri);
     fetch (geocodeUri) 
@@ -43,13 +65,32 @@ function geocode(event) {
             return response.json();
         })
         .then(function(data) {
-            return getFiveDayForecast(data);
+            weather.fiveday = getFiveDayForecast(data);
         })
-    searchBar.value = "";
+        .then(function () {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latLong[0]}&lon=${latLong[1]}&units=imperial&appid=${apiKey}`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                weather.today = currentDay(data)
+            })
+            console.log(weather);
+            searchBar.value = "";
+            return weather;
+        })
+
+    
+}
+
+function currentDay (todaysData) {
+    console.log(todaysData)
+    cityDateText.textContent = `${todaysData.name} (${currentDate()})`
 }
 
 
 function getFiveDayForecast(forecastData) {
+    forecastSection.innerHTML = '';
     console.log(`Weather data!!:\n${forecastData.list[0].dt}`);
     var fullForecast = forecastData.list
     var trimmedFiveDay = []
@@ -72,7 +113,8 @@ function printFiveDayForecast (forecast) {
     for (let i = 0; i < forecast.length; i++) {
         var dayOfWeather = forecast[i];
         var forecastCard = document.createElement("div")
-        forecastCard.classList.add("card", "col-12", "col-md-6", "col-sm-12", "text-light", "bg-dark");
+
+        forecastCard.classList.add("card", "col-12", "col-lg-2", "text-light", "bg-dark", "mb-2", "mx-2");
 
         for (const weatherType in dayOfWeather) {
             console.log(`${weatherType}: ${dayOfWeather[weatherType]}`);
@@ -84,6 +126,7 @@ function printFiveDayForecast (forecast) {
             else {
                 var condition = document.createElement("span");
                 condition.textContent = `${weatherType}${dayOfWeather[weatherType]}`;
+                condition.classList.add("font-weight-light");
                 forecastCard.append(condition)
             }
         }
